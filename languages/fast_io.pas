d15742eee44f92e240105a776de7e60dc5126d44
William Di Luigi <williamdiluigi@gmail.com>
2015-09-13 16:10:01 +0200

    Refactor names and support negative numbers

--- languages/fast_io.pas
+++ languages/fast_io.pas
@@ -2,46 +2,95 @@ uses Classes;
 
 const MAXBUF = 4096 * 4;
 var
-    TotalBytesRead, BytesRead : int64;
-    Buffer : array [0..MAXBUF] of char;
-    idxBuffer : longint;
-    FileStream : TFileStream;
+    total_bytes_read, bytes_read : int64;
+    buffer : array [0..MAXBUF] of char;
+    idx_buffer : longint;
+    file_stream : TFileStream;
 
-function getChar() : char;
+function read_fast_char() : char;
 begin
-    getChar := Buffer[idxBuffer];
-    if idxBuffer = MAXBUF then (* Sono all'ultimo carattere: leggo un altro pezzo di roba *)
+    (* Take one byte out of the buffer *)
+    read_fast_char := buffer[idxBuffer];
+
+    if idx_buffer = MAXBUF then (* I'm at the end of the buffer, read another buffer *)
     begin
-        if TotalBytesRead <= FileStream.Size then (* Il file NON e' ancora terminato *)
+        if total_bytes_read <= file_stream.Size then (* We haven't reached EOF *)
         begin
-            BytesRead := FileStream.Read(Buffer, sizeof(Buffer));
-            inc(TotalBytesRead, BytesRead);
+            bytes_read := file_stream.Read(buffer, sizeof(Buffer));
+            inc(total_bytes_read, bytes_read);
         end;
-        idxBuffer := 0;
-    end else
-        inc(idxBuffer);
+
+        idx_buffer := 0;
+    end
+    else (* The buffer has not ran out of bytes, yet *)
+    begin
+        inc(idx_buffer);
+    end;
 end;
 
-function nextInt() : longint;
+function fast_read_int() : longint;
 var res : longint;
     c : char;
+    negative : boolean;
 begin
     res := 0;
+    negative := False;
+
     repeat
-        c := getChar();
-    until ('0' <= c) and (c <= '9');
+        c := read_fast_char();
+    until (c = '-') or (('0' <= c) and (c <= '9'));
+
+    if c = '-' then
+    begin
+        negative := True;
+        c := read_fast_char();
+    end;
+
     repeat
         res := res * 10 + ord(c) - ord('0');
-        c := getChar();
+        c := read_fast_char();
     until not (('0' <= c) and (c <= '9'));
-    nextInt := res;
+
+    if negative then
+        fast_read_int := -res;
+    else
+        fast_read_int := res;
 end;
 
-procedure initFastinput(inputName : string);
+function fast_read_longint() : int64;
+var res : int64;
+    c : char;
+    negative : boolean;
 begin
-    FileStream := TFileStream.Create(inputName, fmOpenRead);
-    FileStream.Position := 0;
-    BytesRead := FileStream.Read(Buffer, sizeof(Buffer));
-    inc(TotalBytesRead, BytesRead);
-    idxBuffer := 0;
+    res := 0;
+    negative := False;
+
+    repeat
+        c := read_fast_char();
+    until (c = '-') or (('0' <= c) and (c <= '9'));
+
+    if c = '-' then
+    begin
+        negative := True;
+        c := read_fast_char();
+    end;
+
+    repeat
+        res := res * 10 + ord(c) - ord('0');
+        c := read_fast_char();
+    until not (('0' <= c) and (c <= '9'));
+
+    if negative then
+        fast_read_int := -res;
+    else
+        fast_read_int := res;
+end;
+
+procedure init_fast_io(input_name : string);
+begin
+    file_stream := TFileStream.Create(input_name, fmOpenRead);
+    file_stream.Position := 0;
+    bytes_read := file_stream.Read(buffer, sizeof(Buffer));
+    inc(total_bytes_read, bytes_read);
+    idx_buffer := 0;
 end;
