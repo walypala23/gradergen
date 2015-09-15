dda8c82bebdf87d6ef7adbe62b0fa5b4eb12b931
William Di Luigi <williamdiluigi@gmail.com>
2015-09-15 11:08:57 +0200

    Ops

---
+++ testing/test2/nome_sorgente_contestant.cpp
@@ -0,0 +1,19 @@
+void cerca(int N, char mat[5000][5000], int* A, int* B, int* C) {
+    for (int i=0; i<N; i++) 
+        for (int j=0; j<N; j++) {
+            int val = mat[i][j] - '0';
+        }
+    for (int i=0; i<N; i++)
+        for (int j=i+1; j<N; j++)
+            for (int k=j+1; k<N; k++) {
+                bool cor = mat[i][j] && mat[j][k] && mat[k][i];
+                cor |= mat[i][k] && mat[k][j] && mat[j][i];
+                if (cor) {
+                    *A = i+1;
+                    *B = j+1;
+                    *C = k+1;
+                    return;
+                }
+            }
+    *A = *B = *C = -1;
+}
