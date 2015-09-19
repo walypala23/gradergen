7dcd38b95d1d5161c018a1bcb1b2656925e3bb5a
Federico Glaudo <dario2994@gmail.com>
2015-09-20 00:25:52 +0200

    Aggiunto test5

---
+++ testing/test4/nome_sorgente_contestant.pas
@@ -0,0 +1,15 @@
+unit nome_sorgente_contestant;
+
+interface
+
+type
+
+function moltiplica(A: Longint; B: Longint): Longint;
+
+implementation
+function moltiplica(A : Longint; B : Longint): Longint;
+begin
+    moltiplica := A*B;
+end;
+
+end.
